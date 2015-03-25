653c0fc9abdd083cd16233415d19c9bc90546e8c
Lie2Me <hjs_hejinshan@163.com>
2015-03-25 23:57:14 +0800

    init repo

---
+++ src/Test1/__init__.py
@@ -0,0 +1,11 @@
+name = raw_input("Please input your name:\n")
+if (name.endswith('tank')) :
+    print 'Hello tank'
+elif name.endswith("xiao") :
+    print 'hello xiao'
+else :
+    print 'hello X-man'
+
+
+if __name__=="__main__":
+    print "THis is first in function!!\n"
